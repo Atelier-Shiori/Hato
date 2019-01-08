36af8fd484ce3f8a2fd19888d15fac089ee09624
香風智乃 <koufuuchino@koufuuchinos-MacBook-puro.local>
2019-01-07 19:41:25 -0500

    Add instance setting to python script

--- tools/primehato.py
+++ tools/primehato.py
@@ -2,6 +2,7 @@ import sys
 import requests
 import time
 
+instance = "http://localhost:50420"
 filename = "Filename:" sys.argv[1]
 site = "Site Source:" sys.argv[2]
 mediatype = "Media Type" + sys.argv[3]
@@ -18,7 +19,7 @@ if len(filename) > 0 and (mediatype == "anime" or mediatype == "manga") and len(
             titleids.append(currentid)
         
     for titleid in titleids:
-        URL = "https://hato.malupdaterosx.moe/api/mappings/" + site + "/" + mediatype + "/" + titleid
+        URL = instance + "/api/mappings/" + site + "/" + mediatype + "/" + titleid
         request = requests.get(url = URL)
         data = request.json()
         print(data);
