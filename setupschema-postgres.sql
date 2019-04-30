a357b217c9f51e80dcb00f343af25c1f3efc384e
香風智乃 <koufuuchino@koufuuchinos-MacBook-puro.local>
2019-04-30 13:48:25 -0400

    Revise Postgres setup sql script.

--- setupschema-postgres.sql
+++ setupschema-postgres.sql
@@ -1,17 +1,41 @@
-CREATE TABLE "apikeys" (
-  "id" int NOT NULL,
-  "apikey" varchar(100) NOT NULL,
-  "enabled" int NOT NULL,
-  PRIMARY KEY ("id")
-);
-CREATE TABLE "titleids" (
-  "id" BIGSERIAL,
-  "anidb_id" int DEFAULT NULL,
-  "anilist_id" int DEFAULT NULL,
-  "kitsu_id" int DEFAULT NULL,
-  "malid" int DEFAULT NULL,
-  "animeplanet_id" varchar(50) DEFAULT NULL,
-  "notify_id" varchar(30) DEFAULT NULL,
-  "mediatype" int NOT NULL,
-  PRIMARY KEY ("id")
-);
+CREATE DATABASE hato
+    WITH 
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LC_COLLATE = 'English_United States.1252'
+    LC_CTYPE = 'English_United States.1252'
+    TABLESPACE = pg_default
+    CONNECTION LIMIT = -1;
+	
+CREATE TABLE apikeys
+(
+    id integer NOT NULL,
+    apikey character varying(100) COLLATE pg_catalog."default" NOT NULL,
+    enabled integer NOT NULL,
+    CONSTRAINT apikeys_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+
+CREATE TABLE titleids
+(
+    id bigint NOT NULL DEFAULT nextval('titleids_id_seq'::regclass) ( INCREMENT 1 START 1 MINVALUE 1 MAXVALUE 9223372036854775807 CACHE 1 ),
+    anidb_id integer,
+    anilist_id integer,
+    kitsu_id integer,
+    malid integer,
+    animeplanet_id character varying(50) COLLATE pg_catalog."default" DEFAULT NULL::character varying,
+    notify_id character varying(30) COLLATE pg_catalog."default" DEFAULT NULL::character varying,
+    mediatype integer NOT NULL,
+    CONSTRAINT titleids_pkey PRIMARY KEY (id)
+)
+WITH (
+    OIDS = FALSE
+)
+
+GRANT CREATE, CONNECT ON DATABASE hato TO <hato db username>;
+GRANT INSERT, SELECT, UPDATE, DELETE ON TABLE apikeys TO <hato db username>;
+GRANT INSERT, SELECT, UPDATE ON TABLE titleids TO <hato db username>;
+Grant USAGE, SELECT ON ALL SEQUENCES IN SCHEMA public TO <hato db username>;
+commit;
